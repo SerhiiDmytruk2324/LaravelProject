099001481963bb9bcc8a79efc7db15dba21a2209
Serhii Dmytruk <dmytruk.serhii@gmail.com>
2021-02-21 16:30:27 +0200

    55 Коллекции

---
+++ app/Http/Controllers/DiggingDeeperController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BlogPost;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class DiggingDeeperController extends Controller
+{
+    /**
+     * Базовая информация
+     * @url https://laravel.com/docs/5.8/collections
+     *
+     * Справочная информация
+     * @url https://laravel.com/api/5.8/Illuminate/Support/Collection.html
+     *
+     * Вариант коллекции для моделей eloquent:
+     * @url https://laravel.com/api/5.8/Illuminate/Database/Eloquent/Collection.html
+     *
+     * Билдер запросов - то с чем можно перепутать коллекции:
+     * @url https://laravel.com/docs/5.8/queris
+     */
+    public function collections()
+    {
+        $result = [];
+
+        /**
+         * @var \Illuminate\Database\Eloquent\Collection $eloquentCollection
+         */
+        $eloquentCollection = BlogPost::withTrashed()->get();
+        //dd(__METHOD__, $eloquentCollection, $eloquentCollection->toArray());
+        /**
+         * @var \Illuminate\Support\Collection $collection
+         */
+        $collection = collect($eloquentCollection->toArray());
+
+//        dd(
+//            get_class($eloquentCollection),
+//            get_class($collection),
+//            $collection
+//        );
+
+//        $result['first'] = $collection->first();
+//        $result['last'] = $collection->last();
+
+        $result['where']['data'] = $collection
+            ->where('category_id', 10)
+            ->values()
+            ->keyBy('id');
+
+//        $result['where']['count'] = $result['where']['data']->count();
+//        $result['where']['isEmpty'] = $result['where']['data']->isEmpty();
+//        $result['where']['isNotEmpty'] = $result['where']['data']->isNotEmpty();
+
+//        $result['where_first'] = $collection
+//            ->firstWhere('created_at', '>0', '2019-01-17 01:35:11');
+
+        //Базовая переменная не изменится. Вернется измененная версия.
+//        $result['map']['all'] = $collection->map(function (array $item) {
+//            $newItem = new \stdClass();
+//            $newItem->item_id = $item['id'];
+//            $newItem->item_name = $item['title'];
+//            $newItem->exists = is_null($item['deleted_at']);
+//
+//            return $newItem;
+//        });
+//
+//        $result['map']['not_exists'] = $result['map']['all']
+//            ->where('exists', '=', false)
+//            ->values()
+//            ->keyBy('item_id');
+
+        //Базовая переменная изменится (трансформируется).
+        $collection->transform(function (array $item) {
+            $newItem = new \stdClass();
+            $newItem->item_id = $item['id'];
+            $newItem->item_name = $item['title'];
+            $newItem->exists = is_null($item['deleted_at']);
+            $newItem->created_at = Carbon::parse($item['created_at']);
+
+            return $newItem;
+        });
+
+//        $newItem = new \stdClass();
+//        $newItem->id = 9999;
+//
+//        $newItem2 = new \stdClass();
+//        $newItem2->id = 8888;
+
+        //Установить елемент в начало коллекции
+//        $newItemFirst = $collection->prepend($newItem)->first();
+//        $newItemLast = $collection->push($newItem2)->last();
+//        $pulledItem = $collection->pull(1);
+//
+//        dd(compact('collection', 'newItemFirst','newItemLast','pulledItem'));
+
+//        //Фильтрация. Замена orWhere()
+//        $filtered = $collection->filter(function ($item) {
+//            $byDay = $item->created_at->isFriday();
+//            $byDate = $item->created_at->day == 11;
+//
+//            $result = $byDay && $byDate;
+//
+//            return $result;
+//        });
+//
+//        dd(compact('filtered'));
+
+//        $sortedSimpleCollection = collect([5, 3, 1, 2, 4])->sort()->values();
+//        $sortedAscCollection = $collection->sortBy('created_at');
+//        $sortedDescCollection = $collection->sortByDesc('item_id');
+//
+//        dd(compact('sortedSimpleCollection', 'sortedAscCollection', 'sortedDescCollection'));
+    }
+}
